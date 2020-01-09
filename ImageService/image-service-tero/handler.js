02015f4ece4f80a60f885f32cd6e528f315c1aaf
unknown <tero.nordstrom@sadeinnovations.com>
2020-01-09 15:30:40 +0200

    End result for exercise VIII

--- ImageService/image-service-tero/handler.js
+++ ImageService/image-service-tero/handler.js
@@ -1,6 +1,26 @@
 'use strict';
+const AWS = require('aws-sdk');
+const client = new AWS.Rekognition();
 
-module.exports.analyzeImage = (event, context, _callback) => {
+module.exports.analyzeImage = async event => {
   console.log('Received event for file, content ' + JSON.stringify(event));
-  console.log('Context ' + JSON.stringify(context));
+  const params = {
+    Image: {
+      S3Object: {
+        Bucket: event.Records[0].s3.bucket.name,
+        Name: event.Records[0].s3.object.key,
+      },
+    },
+    MaxLabels: 10,
+    MinConfidence: 70,
+  }
+
+  console.log('Analyzing file, bucket ' + params.Image.S3Object.Bucket + ', name ' + params.Image.S3Object.Name);
+
+  try {
+    var result = await client.detectLabels(params).promise();
+    console.log('Result ' + JSON.stringify(result));
+  } catch (e) {
+    console.error('Label detect failed, error ' + e);
+  }
 };
